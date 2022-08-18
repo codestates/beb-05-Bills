b50631a65aad86ef08051c73b76a02e925c70371
JSND-OJ00 <jsnd.oj@gmail.com>
2022-08-18 10:46:21 +0900

    test

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 db.sequelize
-  .sync()
+  .sync({ force: false })
   .then(() => {
     console.log("db 연결 성공");
   })
@@ -32,7 +32,7 @@ app.use(
   })
 );
 app.use(morgan("dev"));
-app.use(session({  secret: '@bills'}));
+app.use(session({ secret: "@bills" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(express.json()); // axios 통신 : req.body에 프론트의 데이터를 json 형식으로 담아 줌.
 app.use(express.urlencoded({ extended: true })); // urlencoded 방식으로 넘어온 form submit 데이터를 qs 라이브러리로 해독(multipart 아님)
