ce7f4b34813ee3482d7c0d3bd20191d12d1a423d
donggni0712 <donggni0712@naver.com>
2022-08-22 17:05:25 +0900

    :memo:Add Description

--- smart-contract/README.md
+++ smart-contract/README.md
@@ -1,19 +1,19 @@
 # SmartContract
 
+## /contracts
+
 ![](./img/contractFunction.png)
+
+## /migrations
+
+![](./img/migrations.png)
 
 # 실행 방법
 
-## 1. 의존성 설치 및 컨트랙트 배포
+## 1. 의존성 설치 및 컨트랙트 배포, 초기 설정
 
 ```
 npm install
 truffle compile
 truffle migrate --reset
-```
-
-## 2. 초기 설정
-
-```
-
 ```
--- smart-contract/js/setMarketAddr.js
+++ smart-contract/js/setMarketAddr.js
@@ -5,6 +5,6 @@ const serverAddr = "0x1ACa50DF5C7284a411E14c0609EA42E85f97B327"
 let web3 = new Web3('http://127.0.0.1:7545');
 var myContract = new web3.eth.Contract(tokenABI, tokenContract);
 
-myContract.methods.setMarketAddr("0xF95985c40C500b8ee3e76cb99577F1bDEC70b863").send({from: serverAddr});
+// myContract.methods.setMarketAddr("0xF95985c40C500b8ee3e76cb99577F1bDEC70b863").send({from: serverAddr});
 
 myContract.methods.marketAddr().call().then((el)=>{console.log(el)});
